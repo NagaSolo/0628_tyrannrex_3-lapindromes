ce86ceac274ec6d73704e78fb5fcd2f38219c85f
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-07-01 10:31:09 +0100

    add splitting string to half, add files to be ignored

---
+++ palindrome.py
@@ -0,0 +1,26 @@
+"""Input:
+        First line of input contains a single integer T, the number of test cases.
+        Each test is a single line containing a string S composed of only lowercase English alphabet.
+
+    Output:
+        For each test case, output on a separate line: "YES" if the string is a lapindrome and "NO" if it is not.
+        
+    Constraints:
+        1 ≤ T ≤ 100
+        2 ≤ |S| ≤ 1000, where |S| denotes the length of S
+"""
+test_string = 'abcdcba'
+string_length = len(test_string)
+print (string_length)
+# using string comprehension; split the string
+part_one = test_string[0:string_length//2 if string_length%2 == 0 else ((string_length//2) + 1)]
+part_two = test_string[string_length//2 if string_length%2 == 0 else ((string_length//2) + 1):]
+print(part_one)
+print(part_two)
+
+# T = int(input())
+# while T>=1 & T<=100:
+#     if input_string == palindrome:
+#         print('YES')
+#     else:
+#         print('NO')
