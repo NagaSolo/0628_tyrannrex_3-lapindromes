12435a2b30ba77f5b9a6679b535ef5e10f933b83
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-07-30 17:09:01 +0100

    implement; short solution using functional paradigm

---
+++ palindrome_short.py
@@ -0,0 +1,29 @@
+''' finding the shortest solution to the palindrome challenges
+
+    Input:
+        First line of input contains a single integer T, the number of test cases.
+        Each test is a single line containing a string S composed of only lowercase English alphabet.
+
+    Output:
+        For each test case, output on a separate line: "YES" if the string is a lapindrome and "NO" if it is not.
+        
+    Constraints:
+        1 ≤ T ≤ 100
+        2 ≤ |S| ≤ 1000, where |S| denotes the length of S
+'''
+
+# def palindrome(input_string):
+#     if input_string == reversed(input_string): 
+#         return 'YES'
+#     else:
+#         return 'NO'
+
+palindrome = lambda input_string: sorted(input_string[:len(input_string)//2]) == sorted(input_string[len(input_string)//2:])
+
+# driver
+if __name__ == '__main__':
+    T = int(input())
+    for test in range(T):
+        S = input()
+        print(palindrome(S))
+
